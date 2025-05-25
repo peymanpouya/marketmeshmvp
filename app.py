e64df6e415c3404214be8766d515bb542adb5d4d
peymanpouya <ppuyanejad@gmail.com>
2025-05-25 11:18:44 +0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import jsonify
 if __name__ == '__main__':
     app.run(debug=True)
 app = Flask(__name__, template_folder='templates')
-# @app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 @app.route('/fit-score', methods=['POST'])
 # @app.route('/', methods=['POST'])
 
